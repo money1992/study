a02ba2b63a258b7656739a98a1caf2f17fc47854
efafkon <konrad.faff@ericsson.com>
2020-03-23 14:48:39 +0100

    Initial commit

---
+++ parse_csv.py
@@ -0,0 +1,16 @@
+import csv
+
+with open("names.csv", 'r') as csv_file:  
+    csv_reader = csv.reader(csv_file)   
+    
+    #next(csv_reader) startuje od drugiej linijki 
+    with open('new_names.csv', 'w') as new_file:
+        csv_writer = csv.writer(new_file, delimiter = '-')
+
+        for line in csv_reader:
+            csv_writer.writerow(line)
+        
+        
+
+        
+
